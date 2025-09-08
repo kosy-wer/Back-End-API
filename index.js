17928bba0c701122e9666465a0471f7404e09413
kosy-wer <tankerjohnsonmustang.com>
2025-09-08 11:28:52 +0700

    fix bug free

--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello World from me  ');
-});
-
-server.listen(process.env.PORT || 3000);
-
